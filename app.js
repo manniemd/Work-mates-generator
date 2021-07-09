28d5c1dbe6ea04d13edf37be9d04b55359d24ac7
manuel duron <manniemd@gmail.com>
2021-07-08 20:06:13 -0400

    adding questions

--- app.js
+++ app.js
@@ -10,3 +10,57 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRender.js");
 const { ENGINE_METHOD_RSA } = require("constants");
 const writetoFile = util.promisify(fs.writeFile);
+
+//variables
+const managers = [];
+const interns = [];
+const engineers = [];
+let ismanagerPicked = false;
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter employee name.",
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "Employee title: ",
+            choices: ismanagerPicked ? ["Engineer", "Intern"] : ["Manager", "Engineer", "Intern"]
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Employee ID:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Employee's email address:",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Office number:",
+            when: (answers) => answers.role === "Manager"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "GithubID:",
+            when: (answers) => answers.role === "Engineer"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Name of school:",
+            when: (answers) => answers.role === "Intern"
+        },
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Would you like to enter another employee?"
+        }
+    ]);
+}
